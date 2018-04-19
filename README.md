734cc3bc3669aa720fce48998fb4fba718701f71
wukgdu <wukgdu365@yahoo.com>
2018-04-19 23:29:49 +0800

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 23333
 333332
+[![Build status](https://ci.appveyor.com/api/projects/status/github/headacheboy/23333?svg=true)](https://ci.appveyor.com/project/wukgdu/23333/branch/master)
--- test.pl
+++ test.pl
@@ -2,12 +2,15 @@ chdir('lab2');
 system("del /S *.class");
 system('javac Main.java -encoding utf-8');
 my @files = ("BinaryTree", "BubbleSort", "Factorial", "LinearSearch", "LinkedList", "MoreThan4", "QuickSort", "TreeVisitor");
+my $exit_code = 0;
 for my $i (@files) {
     print "$i\n";
     my $out_pg = `java Main ../ztest/$i.java > ../run/tmp.pg`;
     my $out = `java -jar ../run/pgi.jar < ../run/tmp.pg`;
     my $ans = `java -jar ../run/pgi.jar < ../run/$i.pg`;
     if ($out ne $ans) {
-        exit(10086);
+        print("**************** error\n");
+        $exit_code = 1;
     }
 }
+exit($exit_code);
